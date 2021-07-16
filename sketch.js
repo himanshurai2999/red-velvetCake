0e722b23616cc12f6d1d4d8c9fa6d49d5a7b4c50
himanshurai2999 <77000527+himanshurai2999@users.noreply.github.com>
2021-07-16 04:55:23 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+Burger recipe{
+    Preheat an outdoor grill for high heat and lightly oil grate.
+    In a medium bowl, whisk together egg, salt and pepper. Place ground beef and bread crumbs into the mixture. ...
+    Place patties on the prepared grill. Cover and cook 6 to 8 minutes per side, or to desired doneness.
+}
+
